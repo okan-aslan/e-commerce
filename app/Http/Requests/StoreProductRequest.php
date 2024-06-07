f0420a85dc4323d4242e1a81e2b2b0336f660486
Okan Aslan <aaslan.okan@gmail.com>
2024-06-07 14:02:28 +0300

    created request & added validation for admin & seller

---
+++ app/Http/Requests/StoreProductRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return auth()->check() && (auth()->user()->isAdmin() || auth()->user()->isSeller());
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'description' => ['nullable', 'string'],
+            'price' => ['required', 'numeric', 'min:1'],
+            'stock' => ['required', 'integer', 'min:1'],
+            'category_id' => ['required', 'exists:categories,id'],
+        ];
+    }
+}
