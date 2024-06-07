c27edb61d40c97801d365018f23d3c8191ceead4
Okan Aslan <aaslan.okan@gmail.com>
2024-06-07 14:02:41 +0300

    created resources

---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'category_id' => $this->category_id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'price' => $this->price,
+            'stock' => $this->stock,
+            'created_at' => $this->created_at,
+            'user' => new UserResource($this->user),
+            'category' => new CategoryResource($this->category),
+        ];
+    }
+}
