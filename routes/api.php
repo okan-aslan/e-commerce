6943302dbe180bf28722b354db79e3917c9fc39b
Okan Aslan <aaslan.okan@gmail.com>
2024-06-08 20:08:26 +0300

    added order routes & seperated product's index & show method from auth:sanctum middleware

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -14,18 +15,21 @@ Route::get('/user', function (Request $request) {
 // Public Routes
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+Route::apiResource('products', ProductController::class)->only(['index', 'show']);
 
 // Protected Routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/profile', [AuthController::class, 'profile']);
-    Route::apiResource('products', ProductController::class);
+
     Route::apiResource('categories', CategoryController::class);
 
     Route::get('cart', [CartController::class, 'index']);
-    
     Route::post('/cart/add', [CartController::class, 'addProduct']);
     Route::post('/cart/remove', [CartController::class, 'removeProduct']);
     Route::post('/cart/empty', [CartController::class, 'emptyCart']);
 
+    Route::get('/orders', [OrderController::class, 'index']);
+    Route::get('/orders/{order}', [OrderController::class, 'show']);
+    Route::post('/orders', [OrderController::class, 'createOrder']);
 });
