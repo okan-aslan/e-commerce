1cd5d5c74f02f1cfbf2bfa67f8102cc282190710
Okan Aslan <aaslan.okan@gmail.com>
2024-06-08 14:16:39 +0300

    define CartItem model with cart and product & cart relationships

---
+++ app/Models/CartItem.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class CartItem extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['cart_id', 'product_id', 'quantity'];
+
+    /**
+     * Get the cart that owns the CartItem
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function cart(): BelongsTo
+    {
+        return $this->belongsTo(Cart::class);
+    }
+
+    /**
+     * Get the product that owns the CartItem
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
