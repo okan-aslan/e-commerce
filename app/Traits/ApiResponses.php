cc26f634b40bb993c38a3f396f9c4c634006cdeb
Okan Aslan <aaslan.okan@gmail.com>
2024-06-08 15:38:29 +0300

    added phpdocs & return types for methods & added types for parameters

--- app/Traits/ApiResponses.php
+++ app/Traits/ApiResponses.php
@@ -2,9 +2,19 @@
 
 namespace App\Traits;
 
+use Illuminate\Http\JsonResponse;
+
 trait ApiResponses
 {
-    protected function success($data, $message = null, $statusCode = 200)
+    /**
+     * Generate a success response.
+     *
+     * @param mixed $data The data to include in the response
+     * @param string|null $message The message to include in the response
+     * @param int $statusCode The HTTP status code
+     * @return \Illuminate\Http\JsonResponse The JSON response
+     */
+    protected function success($data, ?string $message = null, int $statusCode = 200): JsonResponse
     {
         return response()->json([
             'status' => $statusCode,
@@ -13,7 +23,15 @@ trait ApiResponses
         ], $statusCode);
     }
 
-    protected function error($data, $message = null, $statusCode)
+    /**
+     * Generate an error response.
+     *
+     * @param mixed $data The data to include in the response
+     * @param string|null $message The message to include in the response
+     * @param int $statusCode The HTTP status code
+     * @return \Illuminate\Http\JsonResponse The JSON response
+     */
+    protected function error($data, ?string $message = null, int $statusCode): JsonResponse
     {
         return response()->json([
             'status' => $statusCode,
