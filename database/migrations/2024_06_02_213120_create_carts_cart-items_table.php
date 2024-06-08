e0e65dc75698f566a04941569cb0261575d9e574
Okan Aslan <aaslan.okan@gmail.com>
2024-06-08 14:17:28 +0300

    fix typo in quantity column & added default value total_price for carts

--- database/migrations/2024_06_02_213120_create_carts_cart-items_table.php
+++ database/migrations/2024_06_02_213120_create_carts_cart-items_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
-            $table->decimal('total_price', 12);
+            $table->decimal('total_price', 12)->default(0);
             $table->timestamps();
         });
 
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('cart_id')->constrained('carts')->cascadeOnDelete();
             $table->foreignId('product_id')->constrained('products')->cascadeOnDelete();
-            $table->integer('quantitiy');
+            $table->integer('quantity');
             $table->timestamps();
         });
     }
